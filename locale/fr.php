c40eb225db938abed954ca4e09622bf1b6f74d34
levincem <zevince_m@yahoo.fr>
2019-04-03 20:16:15 +0200

    Compatibility with 2.8. Added an end date for the events Modified the month view to use fullcalendar (must upload fullcalendar by yourself, v4)

---
+++ locale/fr.php
@@ -0,0 +1,18 @@
+<?php
+$Definition['Jan'] = 'Janvier';
+$Definition['Feb'] = 'Février';
+$Definition['Mar'] = 'Mars';
+$Definition['Apr'] = 'Avril';
+$Definition['May'] = 'Mai';
+$Definition['Jun'] = 'Juin';
+$Definition['Jul'] = 'Juillet';
+$Definition['Aug'] = 'Août';
+$Definition['Sep'] = 'Septembre';
+$Definition['Oct'] = 'Octobre';
+$Definition['Nov'] = 'Novembre';
+$Definition['Dec'] = 'Décembre';
+$Definition['EventCalendar.DateOrder'] = 'day,month,year';
+$Definition['EventCalendar.DateFormat'] = '%A, %e %B %Y';
+$Definition['Calendar for %B %Y'] = '%B %Y';
+$Definition['EventCalendar.SettingsTitle'] = 'Event Calendar Settings';
+$Definition['EventCalendar.SettingsDescription'] = 'Creation of events can be regulated by category <strong>and</strong> user role. You can set up the categories here, but don\'t forget to assign some permissions in the <a href="/index.php?p=dashboard/role">standard permission section</a> in the dashboard, otherwise you users wouldn\'t be able to use this plugin!';
